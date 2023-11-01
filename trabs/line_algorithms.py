1802b5f2a1fb8d5fc60cbf4ee705bf69d46346ae
Guilherme Bernardo <bguilherme51@gmail.com>
2023-11-01 01:33:56 -0400

    flood fill finalizado

--- trabs/line_algorithms.py
+++ trabs/line_algorithms.py
@@ -15,7 +15,25 @@ def analytic(plane, p1, p2):
         y = m * x + b
         plane.switch_pixel(x, int(y + 0.5))
 
-    plane.print("Algoritmo: Método Analítico")
+    # plane.print("Algoritmo: analitico")
+
+def analyticRemember(plane, p1, p2):
+    if p1.x == p2.x:
+        for y in range(p1.y, p2.y + 1):
+            plane.switch_pixel(p1.x, y)
+        plane.print("Algoritmo: Método Analítico")
+        return
+
+    m = (p2.y - p1.y) / (p2.x - p1.x)
+    b = p2.y - m * p2.x
+
+    for x in range(p1.x, p2.x + 1):
+        y = m * x + b
+        plane.switch_pixel(x, int(y + 0.5))
+
+    # plane.print("Algoritmo: analitico")
+
+###################################################
 
 def dda(plane, p1, p2):
     plane.clear()
@@ -34,7 +52,27 @@ def dda(plane, p1, p2):
             plane.switch_pixel(int(x + 0.5), y)
             x = x + inc
 
-    plane.print("Algoritmo: DDA")
+    # plane.print("Algoritmo: DDA")
+
+def ddaRemember(plane, p1, p2):
+    if abs(p2.x - p1.x) > abs(p2.y - p1.y):
+        inc = (p2.y - p1.y) / (p2.x - p1.x)
+        y = p1.y
+
+        for x in range(p1.x, p2.x + 1):
+            plane.switch_pixel(x, int(y + 0.5))
+            y = y + inc
+    else:
+        inc = (p2.x - p1.x) / (p2.y - p1.y)
+        x = p1.x
+
+        for y in range(p1.y, p2.y + 1):
+            plane.switch_pixel(int(x + 0.5), y)
+            x = x + inc
+
+# plane.print("Algoritmo: DDA")
+
+################################################################
 
 def bresenham_line(plane, p1, p2):
     plane.clear()
@@ -64,4 +102,33 @@ def bresenham_line(plane, p1, p2):
             slope += dx
             y += sy
 
-    plane.print("Algoritmo: Bresenham")
+    # plane.print("Algoritmo: Bresenham")
+
+def bresenham_lineRemember(plane, p1, p2):
+    dx = abs(p2.x - p1.x)
+    dy = abs(p2.y - p1.y) * -1
+    sy = 1 if p1.y < p2.y else -1
+    slope = dx + dy
+    x, y = p1.x, p1.y
+
+    while True:
+        plane.switch_pixel(x, y)
+
+        if x == p2.x and y == p2.y:
+            break
+
+        eslope = 2 * slope
+
+        if eslope >= dy:
+            if x == p2.x:
+                break
+            slope += dy
+            x += 1
+
+        if eslope <= dx:
+            if y == p2.y:
+                break
+            slope += dx
+            y += sy
+
+    # plane.print("Algoritmo: Bresenham")
--- trabs/trabs.py
+++ trabs/trabs.py
@@ -1,5 +1,5 @@
 import numpy as np
-from line_algorithms import analytic, dda, bresenham_line
+from line_algorithms import analytic, dda, bresenham_line, bresenham_lineRemember
 from circle_algorithms import parametric, bresenham_circle, symmetrical_increment
 from fill_algorithms import flood_fill
 
@@ -52,6 +52,7 @@ def switch_case_menu():
     p1 = Point(0, 0, 0)
     p2 = Point(0, 0, 0)
     pcirclecenter = Point(25, 25, 0)
+    pcirclecenter2 = Point(20, 10, 0)
 
     while True:
         print("Menu:")
@@ -74,6 +75,7 @@ def switch_case_menu():
             p1 = Point(p1.x, p1.y, 0)
             p2 = Point(p2.x, p2.y, 0)
             analytic(plane, p1, p2)
+            plane.print("Algoritmo: Método Analítico")
         elif choice == "2":
             p1.x = int(input("Enter the x-coordinate of point 1: "))
             p1.y = int(input("Enter the y-coordinate of point 1: "))
@@ -82,6 +84,7 @@ def switch_case_menu():
             p1 = Point(p1.x, p1.y, 0)
             p2 = Point(p2.x, p2.y, 0)
             dda(plane, p1, p2)
+            plane.print("Algoritmo: DDA")
         elif choice == "3":
             p1.x = int(input("Enter the x-coordinate of point 1: "))
             p1.y = int(input("Enter the y-coordinate of point 1: "))
@@ -90,20 +93,90 @@ def switch_case_menu():
             p1 = Point(p1.x, p1.y, 0)
             p2 = Point(p2.x, p2.y, 0)
             bresenham_line(plane, p1, p2)
+            plane.print("Algoritmo: Bresenham")
         elif choice == "4":
             pcircle = input("Enter the radius of the circle: ")
             parametric(plane, pcirclecenter, int(pcircle))
+            plane.print("Algoritmo: Equação Paramétrica")
         elif choice == "5":
             pcircle = input("Enter the radius of the circle: ")
             symmetrical_increment(plane, pcirclecenter, int(pcircle))
+            plane.print("Algoritmo: Incremental com Simetria")
         elif choice == "6":
             pcircle = input("Enter the radius of the circle: ")
             bresenham_circle(plane, pcirclecenter, int(pcircle))
+            plane.print("Algoritmo: Bresenham")
         elif choice == "7":
+            replacement_color = input("Enter the replacement letter (e.g., x, =, ., a, b): ")
+            
+            # square
+            p1 = Point(15, 15, 0)
+            p2 = Point(30, 15, 0)
+            p3 = Point(15, 30, 0)
+            p4 = Point(30, 30, 0)
+            bresenham_lineRemember(plane, p1,p2)
+            bresenham_lineRemember(plane, p2,p4)
+            bresenham_lineRemember(plane, p3,p4)
+            bresenham_lineRemember(plane, p1,p3)
+            plane.print()
+            input()
+            flood_fill(plane, pcirclecenter.x, pcirclecenter.y, replacement_color)
+            input()
+            
+            # circle
             pcircle = input("Enter the radius of the circle: ")
             bresenham_circle(plane, pcirclecenter, int(pcircle))
-            replacement_color = input("Enter the replacement color (e.g., '\033[91mX\033[39m'): ")
+            plane.print()
+            input()
             flood_fill(plane, pcirclecenter.x, pcirclecenter.y, replacement_color)
+            input()
+            plane.clear
+
+            # plane a)
+            plane.clear()
+            p1 = Point(10, 10, 0)
+            p2 = Point(20, 15, 0)
+            p3 = Point(5, 25, 0)
+            p4 = Point(25, 35, 0)
+            p5 = Point(45, 20, 0)
+            p6 = Point(35, 12, 0)
+            bresenham_lineRemember(plane, p1,p2)
+            bresenham_lineRemember(plane, p3,p2)
+            bresenham_lineRemember(plane, p3,p4)
+            bresenham_lineRemember(plane, p4,p5)
+            bresenham_lineRemember(plane, p6,p5)
+            bresenham_lineRemember(plane, p1,p6)
+            plane.print()
+            input()
+            flood_fill(plane, pcirclecenter.x, pcirclecenter.y, replacement_color)
+            input()
+
+            # plane C)
+            plane.clear()
+            p0 = Point(7, 15, 0)
+            p1 = Point(3, 35, 0)
+            p2 = Point(25, 40, 0)
+            p3 = Point(35, 35, 0)
+            p4 = Point(38, 20, 0)
+            p5 = Point(20, 5, 0)
+            p6 = Point(15, 25, 0)
+            p7 = Point(30, 30, 0)
+            p8 = Point(30, 20, 0)
+            bresenham_lineRemember(plane, p1,p0)
+            bresenham_lineRemember(plane, p1,p2)
+            bresenham_lineRemember(plane, p2,p3)
+            bresenham_lineRemember(plane, p3,p4)
+            bresenham_lineRemember(plane, p5,p4)
+            bresenham_lineRemember(plane, p6,p5)
+            bresenham_lineRemember(plane, p6,p7)
+            bresenham_lineRemember(plane, p7,p8)
+            bresenham_lineRemember(plane, p0,p8)
+            plane.print()
+            input()
+            flood_fill(plane, pcirclecenter.x, pcirclecenter.y, replacement_color)
+            flood_fill(plane, pcirclecenter2.x, pcirclecenter2.y, replacement_color)
+            plane.clear()
+            
         elif choice == "0":
             break
         else:
